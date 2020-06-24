d96a912cbdcf610dc98492f3209a68efca1616b3
Ladislas Gabriel <gabriel.ladislas@gmail.com>
2020-06-24 02:19:43 +0200

    init project

---
+++ src/test/java/com/lagab/capoeira/capoeiraportal/CapoeiraPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagab.capoeira.capoeiraportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapoeiraPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
