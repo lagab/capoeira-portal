9c58b535fc3c83356bad4c6465eb594470290b57
Ladislas Gabriel <gabriel.ladislas@gmail.com>
2020-07-06 00:56:15 +0200

    init address

--- src/main/java/com/lagab/capoeira/capoeiraportal/domain/Address.java
+++ src/main/java/com/lagab/capoeira/capoeiraportal/domain/Address.java
@@ -3,18 +3,25 @@ package com.lagab.capoeira.capoeiraportal.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
-@MappedSuperclass
+@Entity
 public abstract class Address implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String street;
+    private String addressLine1;
+    private String addressLine2;
+    private String postcode;
     private String city;
     private String country;
-    private Float latitude;
-    private Float longitude;
+    private Double latitude;
+    private Double longitude;
 }
