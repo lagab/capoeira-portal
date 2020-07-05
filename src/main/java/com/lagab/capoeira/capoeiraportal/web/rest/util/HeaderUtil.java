ea3f6a349e168981d477a9313f8c3e249f40ca00
Ladislas Gabriel <gabriel.ladislas@gmail.com>
2020-07-05 23:34:57 +0200

    setup security

--- src/main/java/com/lagab/capoeira/capoeiraportal/web/rest/util/HeaderUtil.java
+++ src/main/java/com/lagab/capoeira/capoeiraportal/web/rest/util/HeaderUtil.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 
 public final class HeaderUtil {
@@ -13,6 +16,19 @@ public final class HeaderUtil {
     private static final String APPLICATION_NAME = "capoeiraPortal";
 
     private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String applicationName, String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-" + applicationName + "-alert", message);
+
+        try {
+            headers.add("X-" + applicationName + "-params", URLEncoder.encode(param, StandardCharsets.UTF_8.toString()));
+        } catch (UnsupportedEncodingException var5) {
+            ;
+        }
+
+        return headers;
     }
 
     public static HttpHeaders createAlert(String message, String param) {
