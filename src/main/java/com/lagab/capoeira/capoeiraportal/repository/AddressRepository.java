42ed056344b123762ca90dccb2edc2274270f99e
Ladislas Gabriel <gabriel.ladislas@gmail.com>
2021-01-23 15:16:13 +0100

    Remove addressTable

--- src/main/java/com/lagab/capoeira/capoeiraportal/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lagab.capoeira.capoeiraportal.repository;
-
-import com.lagab.capoeira.capoeiraportal.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long> {
-
-}
