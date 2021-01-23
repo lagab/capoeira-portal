ecb959519916ab8d9e6998a3c2e8320e4d90423f
Ladislas Gabriel <gabriel.ladislas@gmail.com>
2021-01-23 15:17:00 +0100

    add role for student and teachers

--- src/main/java/com/lagab/capoeira/capoeiraportal/security/Authorities.java
+++ src/main/java/com/lagab/capoeira/capoeiraportal/security/Authorities.java
@@ -7,4 +7,8 @@ public class Authorities {
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    public static final String STUDENT = "ROLE_STUDENT";
+
+    public static final String TEACHER = "ROLE_TEACHER";
 }
